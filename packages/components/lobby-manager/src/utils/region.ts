vitaly-z/rivetkit

feat(matchmaker): implement matchmaker component

packages/components/matchmaker/src/utils/region.ts
@@ -1,0 +1,21 @@
+import { z } from "zod";
+import type { LobbyBackend } from "../config";
+import { REGIONS as LOCAL_DEVELOPMENT_REGIONS } from "./lobby/backend/local_development";
+import { REGIONS as SERVER_REGIONS } from "./lobby/backend/server";
+import { REGIONS as TEST_REGIONS } from "./lobby/backend/test";
+import { assertUnreachable } from "actor-core/utils";
+
+export const RegionSchema = z.object({
+	slug: z.string(),
+	name: z.string(),
+	latitude: z.number(),
+	longitude: z.number(),
+});
+export type Region = z.infer<typeof RegionSchema>;
+
+export function regionsForBackend(backend: LobbyBackend): Region[] {
+	if ("test" in backend) return TEST_REGIONS;
+	else if ("localDevelopment" in backend) return LOCAL_DEVELOPMENT_REGIONS;
+	else if ("server" in backend) return SERVER_REGIONS;
+	else assertUnreachable(backend);
+}

packages/components/matchmaker/tests/test-utils.ts
@@ -1,0 +1,79 @@
+import { onTestFinished, vi } from "vitest";
+import { type ActorHandle, Client } from "actor-core/client";
+import getPort from "get-port";
+import { type Matchmaker, matchmaker } from "../src/mod";
+import { serve } from "@actor-core/nodejs";
+
+export const ADMIN_TOKEN = "test-admin";
+export const VERSION = "test";
+export const REGION = "test";
+
+export interface SetupTestResult {
+	mm: ActorHandle<Matchmaker>;
+	client: Client;
+}
+
+export async function setupTest(
+	config?: Parameters<typeof matchmaker>[0],
+): Promise<SetupTestResult> {
+	vi.useFakeTimers();
+
+	const defaultConfig = {
+		lobbies: {
+			unreadyExpireAfter: 60000,
+			regions: [REGION],
+			backend: {
+				test: {},
+			},
+		},
+		players: {
+			unconnectedExpireAfter: 60000,
+		},
+		admin: {
+			token: ADMIN_TOKEN,
+		},
+	};
+
+	// Start server with a random port
+	const port = await getPort();
+	const server = serve({
+		actors: {
+			matchmaker: matchmaker(config ?? defaultConfig),
+		},
+		port,
+	});
+	onTestFinished(
+		async () => await new Promise((resolve) => server.close(() => resolve())),
+	);
+
+	// Create client
+	const client = new Client(`http://localhost:${port}`);
+	onTestFinished(async () => await client.dispose());
+
+	// Get actor reference
+	const mm = await client.get<Matchmaker>({ name: "matchmaker" });
+
+	return {
+		mm,
+		client,
+	};
+}
+
+export async function getLobbyToken(
+	mm: ActorHandle<Matchmaker>,
+	adminToken: string,
+	lobbyId: string,
+): Promise<string> {
+	const lobby = await mm.adminGetLobby({ adminToken, lobbyId });
+	return lobby.token;
+}
+
+export async function setLobbyReady(
+	mm: ActorHandle<Matchmaker>,
+	adminToken: string,
+	lobbyId: string,
+): Promise<{ lobbyToken: string }> {
+	const lobbyToken = await getLobbyToken(mm, adminToken, lobbyId);
+	await mm.setLobbyReady({ lobbyToken });
+	return { lobbyToken };
+}

rivet-gg/actor-core

packages/components/lobby-manager/src/utils/region.ts
@@ -1,0 +1,21 @@
+import { z } from "zod";
+import type { LobbyBackend } from "../config";
+import { REGIONS as LOCAL_DEVELOPMENT_REGIONS } from "./lobby/backend/local_development";
+import { REGIONS as RIVET_REGIONS } from "./lobby/backend/rivet";
+import { REGIONS as TEST_REGIONS } from "./lobby/backend/test";
+import { assertUnreachable } from "actor-core/utils";
+
+export const RegionSchema = z.object({
+	slug: z.string(),
+	name: z.string(),
+	latitude: z.number(),
+	longitude: z.number(),
+});
+export type Region = z.infer<typeof RegionSchema>;
+
+export function regionsForBackend(backend: LobbyBackend): Region[] {
+	if ("test" in backend) return TEST_REGIONS;
+	else if ("localDevelopment" in backend) return LOCAL_DEVELOPMENT_REGIONS;
+	else if ("rivet" in backend) return RIVET_REGIONS;
+	else assertUnreachable(backend);
+}

packages/components/lobby-manager/tests/test-utils.ts
@@ -1,0 +1,79 @@
+import { onTestFinished, vi } from "vitest";
+import { type ActorHandle, Client } from "actor-core/client";
+import getPort from "get-port";
+import { type LobbyManager, lobbyManager } from "../src/mod";
+import { serve } from "@actor-core/nodejs";
+
+export const ADMIN_TOKEN = "test-admin";
+export const VERSION = "test";
+export const REGION = "test";
+
+export interface SetupTestResult {
+	mm: ActorHandle<LobbyManager>;
+	client: Client;
+}
+
+export async function setupTest(
+	config?: Parameters<typeof lobbyManager>[0],
+): Promise<SetupTestResult> {
+	vi.useFakeTimers();
+
+	const defaultConfig = {
+		lobbies: {
+			unreadyExpireAfter: 60000,
+			regions: [REGION],
+			backend: {
+				test: {},
+			},
+		},
+		players: {
+			unconnectedExpireAfter: 60000,
+		},
+		admin: {
+			token: ADMIN_TOKEN,
+		},
+	};
+
+	// Start server with a random port
+	const port = await getPort();
+	const server = serve({
+		actors: {
+			"lobby-manager": lobbyManager(config ?? defaultConfig),
+		},
+		port,
+	});
+	onTestFinished(
+		async () => await new Promise((resolve) => server.close(() => resolve())),
+	);
+
+	// Create client
+	const client = new Client(`http://localhost:${port}`);
+	onTestFinished(async () => await client.dispose());
+
+	// Get actor reference
+	const mm = await client.get<LobbyManager>({ name: "lobby-manager" });
+
+	return {
+		mm,
+		client,
+	};
+}
+
+export async function getLobbyToken(
+	mm: ActorHandle<LobbyManager>,
+	adminToken: string,
+	lobbyId: string,
+): Promise<string> {
+	const lobby = await mm.adminGetLobby({ adminToken, lobbyId });
+	return lobby.token;
+}
+
+export async function setLobbyReady(
+	mm: ActorHandle<LobbyManager>,
+	adminToken: string,
+	lobbyId: string,
+): Promise<{ lobbyToken: string }> {
+	const lobbyToken = await getLobbyToken(mm, adminToken, lobbyId);
+	await mm.setLobbyReady({ lobbyToken });
+	return { lobbyToken };
+}